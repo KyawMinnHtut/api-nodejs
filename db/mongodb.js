7bea2ba950c542512bd06e2224b161efa8eaf6b4
Min Htut <kmhtut123@gmail.com>
2024-07-09 20:51:55 +0630

    mongodb url added

--- db/mongodb.js
+++ db/mongodb.js
@@ -2,7 +2,7 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 
 const username = 'your_username';
 const password = 'your_password';
-const url = `mongodb://${username}:${password}@localhost:27017/?authSource=admin`;
+const url = "mongodb://mongo:pMkdkTcokHLuKNeyygzZSuRpaOBRgIOg@mongodb.railway.internal:27017";
 const dbName = 'football';
 
 let client
@@ -11,7 +11,7 @@ export const getDb = async () => {
   let db;
   if (db) return db;
   try {
-    client = await MongoClient.connect('mongodb://localhost:27017', {
+    client = await MongoClient.connect(url, {
       serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
